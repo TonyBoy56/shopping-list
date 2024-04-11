334d9668ae8a456747785276a58e4a1d41f38058
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-04-11 10:38:11 -0700

    add global vars for form data retrieval.

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// global vars for form data retrieval
+
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
