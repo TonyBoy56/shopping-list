ab1aaac3b6922d5ca01a5d1d2471442bd98a70f7
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-04-12 15:45:12 -0700

    change indent spacing to 2 spaces.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
 // global vars for form data retrieval
 
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+
+function addItem(e) {
+  e.preventDefault();
+
+  // Validate Input
+  if (itemInput.value === "") {
+    alert("Please add an item");
+    return;
+  }
+
+  console.log("success");
+}
 
 // Event Listeners, to be added to an initializer function
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener("submit", addItem);
