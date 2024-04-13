56890e58b53be7daa68133f8c0159500751a0776
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-04-12 17:38:27 -0700

    add script.js to the html file to resolve lack of alert messages being received in the console.

--- src/script.js
+++ src/script.js
@@ -1,19 +1,18 @@
 // global vars for form data retrieval
 
-const itemForm = document.getElementById("item-form");
-const itemInput = document.getElementById("item-input");
-const itemList = document.getElementById("item-list");
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
 
 function addItem(e) {
   e.preventDefault();
 
   // Validate Input
-  if (itemInput.value === "") {
-    alert("Please add an item");
+  if (itemInput.value === '') {
+    alert('Please add an item');
     return;
   }
-
-  console.log("success");
+  console.log('success');
 }
 
 // Event Listeners, to be added to an initializer function
